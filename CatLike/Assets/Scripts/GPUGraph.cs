111cbb256e880a37d129ed5e97109e1643ffd20f
xubin <xubin@Yahaha.com>
2021-08-08 10:26:49 +0800

    compute shader

---
+++ CatLike/Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GPUGraph : MonoBehaviour
+{
+    private const int maxResolution = 1000;
+    [SerializeField] private ComputeShader computeShader;
+    [SerializeField] private Material material;
+    [SerializeField] private Mesh mesh;
+    [SerializeField,Range(10,maxResolution)]
+    private int resolution = 10;
+
+    [SerializeField] 
+    private FunctionLibrary.FunctionName function;
+    
+    public enum TransitionMode
+    {
+        Cycle,
+        Random
+    }
+    [SerializeField] private TransitionMode transitionMode;
+    [SerializeField, Min(0f)] 
+    private float functionDuration = 1f,transitionDuration = 1f;
+    
+    private float duration;
+    private bool transitioning;
+    private FunctionLibrary.FunctionName transitionFunxtion;
+
+    private ComputeBuffer postionBuffer;
+
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
+
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId,resolution);
+        computeShader.SetFloat(stepId,step);
+        computeShader.SetFloat(timeId,Time.time);
+        var kernelIndex = (int)function;
+        computeShader.SetBuffer(kernelIndex,positionsId,postionBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(kernelIndex, groups, groups, 1);
+        material.SetBuffer(positionsId,postionBuffer);
+        material.SetFloat(stepId, step);
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution)); 
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds,resolution * resolution);
+    }
+    private void OnEnable()
+    {
+        postionBuffer = new ComputeBuffer(maxResolution * maxResolution,3 * 4);
+    }
+
+    private void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= functionDuration;
+                transitioning = false;
+            }    
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunxtion = function;
+            PickNextFunction();
+        }
+
+        UpdateFunctionOnGPU();
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle
+            ? FunctionLibrary.GetNextFunctionName(function)
+            : FunctionLibrary.GetRandomFunctionName(function);
+    }
+
+    private void OnDisable()
+    {
+        postionBuffer.Release();
+        postionBuffer = null;
+    }
+}
